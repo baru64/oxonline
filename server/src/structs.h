05d3b84cfd49799db7c367de6bddaf49486220e8
baru <bartosz_krakowiak@hotmail.com>
2017-11-27 10:56:39 +0100

    #4

--- server/src/structs.h
+++ server/src/structs.h
@@ -1,3 +1,38 @@
 //structs
+#include "msg.h"
+#define BUF_LEN 10
+#define LIMIT 2
 
-char test_text[] = "Hi!";
+game GAME;
+struct buffer IPCbuffer;
+struct clientSession connections[LIMIT];
+
+typedef struct game
+{
+	char board[9];
+	enum
+	{
+		"O",
+		"X",
+		"draw",
+		"none"
+	} winner;
+} game;
+
+
+struct buffer
+{
+	int readIdx; //id odczytane
+	int writeIdx; //id zapisane
+	uint8_t player[BUF_LEN]; //numer gracza do ktorego ma zostac wyslana wiadomosc
+	struct message_t messages[BUF_LEN];
+};
+
+struct clientSession
+{
+	pthread_t process;
+	int notEmpty;
+	int finished;
+	int fd;
+};
+
